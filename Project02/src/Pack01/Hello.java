325ae9059e0ffb831f4796588afc6ad1bc4bec8e
Jakwon <Jakwon@192.168.25.31>
2021-03-24 20:54:47 +0900

    1

--- Project02/src/Pack01/Hello.java
+++ Project02/src/Pack01/Hello.java
@@ -5,6 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println(1);
 		// https://github.com/jeeankwon/Tiger01
+		System.out.println(2);
+		System.out.println(3);
 	}
 
 }
